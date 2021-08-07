5e07e5423ee6a8681e5b4a9fab0391ae374d548b
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2021-08-07 11:42:38 +0900

    add transaction
    트랜잭션 / CLI 명령 / block, blockchain 구조 변경

--- block.go
+++ block.go
@@ -3,20 +3,57 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"strconv"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
 type Block struct {
 	Timestamp int64
-	Data []byte
+	Transactions []*Transaction
 	PrevBlockHash []byte
 	Hash []byte
-	Nonce         int
+	Nonce  int
 }
 
-func NewBlock (data string, prevBlockHash [] byte)  *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
+
+func NewBlock (transcations []*Transaction, prevBlockHash [] byte)  *Block {
+	block := &Block{time.Now().Unix(), transcations, prevBlockHash, []byte{}, 0}
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 
@@ -26,13 +63,17 @@ func NewBlock (data string, prevBlockHash [] byte)  *Block {
 	return block
 }
 
-func (b *Block) SetHash() {
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
+}
+
+/*func (b *Block) SetHash() {
 	timestamp :=  []byte(strconv.FormatInt(b.Timestamp,  10))
 	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(header)
 
 	b.Hash = hash[:]
-}
+}*/
 
 
 
